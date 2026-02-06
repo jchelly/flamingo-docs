f21cc6df57de4aefad3a9eb8166f1e8af24ddcc7
John Helly <j.c.helly@durham.ac.uk>
2026-02-06 14:44:35 +0000

    Add script to check snaps are available via API

---
+++ scripts/snapshots/check_snapshots.py
@@ -0,0 +1,86 @@
+#!/bin/env python
+
+import numpy as np
+import hdfstream
+import simulation_list as sl
+
+root = hdfstream.open("flamingo", "/FLAMINGO/")
+
+
+
+checks = [
+    # L1_m8 runs
+    {
+        "box"            : "L1_m8",
+        "names"          : ["L1_m8", "L1_m8_DMO"],
+        "nr_snaps"       : 79,
+        "expected_snaps" : sl.L1_m8_snapshots
+    },
+    # L2p8_m9 runs
+    {
+        "box"            : "L2p8_m9",
+        "names"          : ["L2p8_m9", "L2p8_m9_DMO"],
+        "nr_snaps"       : 79,
+        "expected_snaps" : sl.L1_m8_snapshots
+    },
+    # L1_m9 hydro runs
+    {
+        "box"            : "L1_m9",
+        "names"          : sl.L1_m9,
+        "nr_snaps"       : 78,
+        "expected_snaps" : sl.L1_m9_snapshots
+    },
+    # L1_m9 DMO runs
+    {
+        "box"            : "L1_m9",
+        "names"          : sl.L1_m9_DMO,
+        "nr_snaps"       : 78,
+        "expected_snaps" : sl.L1_m9_snapshots
+    },
+]
+
+
+for check in checks:
+
+    box = check["box"]
+    names = check["names"]
+    nr_snaps = check["nr_snaps"]
+    all_snaps = np.arange(nr_snaps, dtype=int)
+    expected_snaps = np.sort(np.asarray(list(check["expected_snaps"].keys()), dtype=int))
+
+    for name in names:
+
+        dirname = name.replace("σ", "sigma").replace("*","star")
+        print(dirname)
+
+        try:
+            snapshots_dir = root[f"{box}/{dirname}/snapshots"]
+        except KeyError:
+            print("  Directory not found")
+            continue
+
+        # Check which snapshots we have
+        snaps_present = []
+        for snap_nr in range(nr_snaps):
+            filename = f"flamingo_{snap_nr:04d}/flamingo_{snap_nr:04d}.0.hdf5"
+            try:
+                snap_file = snapshots_dir[filename]
+            except KeyError:
+                pass
+            else:
+                snaps_present.append(snap_nr)
+
+        snaps_found = np.sort(np.asarray(snaps_present))
+        if (len(snaps_found) == len(all_snaps)) and np.all(snaps_found==all_snaps):
+            print("  All snapshots present")
+        elif (len(snaps_found) == len(expected_snaps)) and np.all(snaps_found==expected_snaps):
+            print("  Expected subset of snapshots present")
+        else:
+            missing = 0
+            for sn in expected_snaps:
+                if sn not in snaps_found:
+                    missing += 1
+            if missing == 0:
+                print("  Have extra snapshots")
+            else:
+                print("  MISSING SNAPSHOTS")
---
+++ scripts/snapshots/simulation_list.py
@@ -0,0 +1,68 @@
+#!/bin/env/python
+
+L1_m9 = [
+    "L1_m9",
+    "fgas+2σ",
+    "fgas-2σ",
+    "fgas-4σ",
+    "fgas-8σ",
+    "M*-1σ",
+    "M*-1σ_fgas-4σ",
+    "Jet",
+    "Jet_fgas-4σ",
+    "Planck",
+    "PlanckNu0p24Var",
+    "PlanckNu0p24Fix",
+    "PlanckNu0p48Fix",
+    "LS8",
+    "PlanckDCDM12",
+    "PlanckDCDM24",
+    "LS8_fgas-8σ",
+    "NoCooling",
+]
+
+L1_m9_DMO = [
+    "L1_m9_DMO",
+    "Planck_DMO",
+    "PlanckNu0p12Var_DMO",
+    "PlanckNu0p24Var_DMO",
+    "PlanckNu0p24Fix_DMO",
+    "PlanckNu0p48Fix_DMO",
+    "LS8_DMO",
+    "PlanckDCDM12_DMO",
+    "PlanckDCDM24_DMO",
+]
+
+# Snapshots where we expect to have full particle data
+L1_m9_snapshots = {
+  9  : 5.00,
+  13 : 4.00,
+  17 : 3.00,
+  37 : 2.00,
+  47 : 1.50,
+  57 : 1.00,
+  62 : 0.75,
+  67 : 0.50,
+  69 : 0.40,
+  71 : 0.30,
+  73 : 0.20,
+  75 : 0.10,
+  77 : 0.00,
+}
+
+# Snapshots where we expect to have full particle data
+L1_m8_snapshots = {
+  10  : 5.00,
+  14 : 4.00,
+  18 : 3.00,
+  38 : 2.00,
+  48 : 1.50,
+  58 : 1.00,
+  63 : 0.75,
+  68 : 0.50,
+  70 : 0.40,
+  72 : 0.30,
+  74 : 0.20,
+  76 : 0.10,
+  78 : 0.00,
+}
