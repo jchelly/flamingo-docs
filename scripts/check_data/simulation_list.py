aebafb65194f482261230629a66252886f3b232b
John Helly <j.c.helly@durham.ac.uk>
2026-02-10 10:10:20 +0000

    Add script to check map dir presence

--- scripts/check_data/simulation_list.py
+++ scripts/check_data/simulation_list.py
@@ -74,48 +74,48 @@ sim_params = [
         "box"            : "L1_m8",
         "names"          : ["L1_m8", "L1_m8_DMO"],
         "nr_snaps"       : 79,
-        "expected_snaps" : sl.L1_m8_snapshots
+        "expected_snaps" : L1_m8_snapshots
     },
     # L1_m10 runs
     {
        "box"            : "L1_m10",
        "names"          : ["L1_m10", "L1_m10_DMO"],
        "nr_snaps"       : 78,
-       "expected_snaps" : sl.L1_m9_snapshots
+       "expected_snaps" : L1_m9_snapshots
     },
     # L2p8_m9 runs
     {
         "box"            : "L2p8_m9",
         "names"          : ["L2p8_m9", "L2p8_m9_DMO"],
         "nr_snaps"       : 79,
-        "expected_snaps" : sl.L1_m8_snapshots
+        "expected_snaps" : L1_m8_snapshots
     },
     # L1_m9 hydro runs
     {
         "box"            : "L1_m9",
-        "names"          : sl.L1_m9,
+        "names"          : L1_m9,
         "nr_snaps"       : 78,
-        "expected_snaps" : sl.L1_m9_snapshots
+        "expected_snaps" : L1_m9_snapshots
     },
     # L1_m9 DMO runs
     {
         "box"            : "L1_m9",
-        "names"          : sl.L1_m9_DMO,
+        "names"          : L1_m9_DMO,
         "nr_snaps"       : 78,
-        "expected_snaps" : sl.L1_m9_snapshots
+        "expected_snaps" : L1_m9_snapshots
     },
     # L5p6_m10 runs
     {
         "box"            : "L5p6_m10",
         "names"          : ["L5p6_m10_DMO"],
         "nr_snaps"       : 79,
-        "expected_snaps" : sl.L1_m8_snapshots
+        "expected_snaps" : L1_m8_snapshots
     },
     # L11p2_m11 runs
     {
         "box"            : "L11p2_m11",
         "names"          : ["L11p2_m11_DMO"],
         "nr_snaps"       : 79,
-        "expected_snaps" : sl.L1_m8_snapshots
+        "expected_snaps" : L1_m8_snapshots
     },
 ]
